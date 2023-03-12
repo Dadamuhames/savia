5f9c28e0f7a85dc3fa2a8e81518818fe039055d0
MSD <dadamuhames@gmail.com>
2023-03-12 17:35:58 +0500

    Fix

--- admins/templatetags/baner_show.py
+++ admins/templatetags/baner_show.py
@@ -35,7 +35,7 @@ def baner_thumb(image, **kwargs):
     if request is not None:
         print(url)
         print(str(url).split('/media/'))
-        final_url = '/media/' + str(url).split('\media\\')[-1].replace('\\', "/") 
+        final_url = '/media/' + str(url).split('/media/')[-1].replace('\\', "/") 
         print(final_url)
         return request.build_absolute_uri(final_url)
 
